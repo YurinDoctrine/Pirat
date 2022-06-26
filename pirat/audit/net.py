d47cd9c2c011f10d3aad2cf3796e194937c2309a
Ivan Nikolsky <enty8080@gmail.com>
2022-06-26 23:35:44 +0200

    Update net.py

--- pirat/audit/net.py
+++ pirat/audit/net.py
@@ -53,7 +53,6 @@ class Net:
     macdb_updated = False
 
     result = {}
-    completed = False
 
     @staticmethod
     def get_gateways() -> dict:
@@ -211,15 +210,6 @@ class Net:
                     }
                 }
             })
-
-        completed = True
-
-    def audit_completed(self) -> bool:
-        """ Check if audit completed.
-        
-        """
-
-        return self.completed
 
     def audit_result(self) -> dict:
         """ Get network audit result.
